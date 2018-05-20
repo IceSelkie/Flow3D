024151338b24f28b55e03887e40a5055d34d91fd
StanleyMines <stanleymines@gmail.com>
2018-05-19 21:46:48 -0700

    Array holder to use and pass small parts of arrays that doesnt allow for modification of the original array. Useful for a few different things.

---
+++ SplittableImmutableIntArray.java
@@ -0,0 +1,96 @@
+import java.util.Iterator;
+
+public final class SplittableImmutableIntArray implements Iterable
+{
+  protected final int[] data;
+  protected final int start;
+  protected final int end;
+  protected final int len;
+
+  public SplittableImmutableIntArray(int[] array)
+  {
+    this(array, 0, array.length);
+  }
+
+  public SplittableImmutableIntArray(int[] array, int start, int end)
+  {
+    this(start, end, array.clone());
+  }
+
+  private SplittableImmutableIntArray(int start, int end, int[] array)
+  {
+    data = array;
+    this.start = start;
+    this.end = end;
+    this.len = end - start;
+
+    if (start < 0)
+      throw new IllegalArgumentException("The array cannot start at a negative index (" + start + ").");
+    if (!(start < end))
+      throw new IllegalArgumentException("The array cannot end before it starts.");
+    if (end >= data.length)
+      throw new IllegalArgumentException("The array isn't long enough to end at " + end + ", it is only " + len + " long.");
+  }
+
+  public int get(int i)
+  {
+    if (i > 0 && i < end)
+      return data[start + i];
+    else
+      throw new ArrayIndexOutOfBoundsException();
+  }
+
+  public SplittableImmutableIntArray split(int start, int end)
+  {
+    if (end >= len)
+      throw new IllegalArgumentException("This array isn't long enough to end at " + end + ". (It can be at most " + (len - 1) + ")");
+    return new SplittableImmutableIntArray(data, this.start + start, start + end);
+  }
+
+  public int length()
+  {
+    return len;
+  }
+
+  public int[] toArray()
+  {
+    int[] array = new int[len];
+    for (int i = 0; i < len; i++)
+      array[i] = data[start + i];
+    return array;
+  }
+
+  @Deprecated
+  public SplittableImmutableIntArray trim()
+  {
+    return new SplittableImmutableIntArray(0, len, this.toArray());
+  }
+
+  @Override
+  public Iterator iterator()
+  {
+    return new SIIAIterator(this);
+  }
+
+  private class SIIAIterator implements Iterator
+  {
+    protected SplittableImmutableIntArray siia;
+    protected int index;
+
+    private SIIAIterator(SplittableImmutableIntArray arr)
+    {
+      siia = arr;
+      index = 0;
+    }
+
+    public boolean hasNext()
+    {
+      return index < siia.length();
+    }
+
+    public Object next()
+    {
+      return siia.get(index++);
+    }
+  }
+}
