fdca421844f7ca50b3f82a8ffc49cda61b8b2438
StanleyMines <stanleymines@gmail.com>
2018-05-19 22:04:41 -0700

    Driver class with main. Doesn't have anything yet.

---
+++ Driver.java
@@ -0,0 +1,44 @@
+import java.awt.*;
+
+/**
+ * class Driver
+ *
+ * Has the main method to start the game of Flow3D.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Driver
+{
+    /**
+     * Main: The game of Flow begins here.
+     *
+     * @param args Command line arguments passed to the program at start. Currently unused.
+     */
+    public static void main(String[] args)
+    {
+        //TODO: Just about everything.
+        runGame();
+    }
+
+
+    /**
+     * Constructor for objects of class Driver
+     */
+    public Driver()
+    {
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param y a sample parameter for a method
+     * @return the sum of x and y
+     */
+    public static void runGame()
+    {
+        // Create display window/canvas
+        // Load menu
+        // wait for callback from menu to do the next thing: tutorial, game, exit, etc.
+    }
+}
