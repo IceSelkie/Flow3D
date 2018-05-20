586b43d5265f2efb7ae6a69eba21d58e8a37e331
StanleyMines <stanleymines@gmail.com>
2018-05-19 22:07:30 -0700

    Display class thing built off of JFrame and JPanel stuff. (WIP)

---
+++ Display.java
@@ -0,0 +1,14 @@
+import javax.swing.JPanel;
+
+/**
+ * class Display
+ *
+ * Uses JFrames to be able to draw objects to the screen and have interactions and buttons
+ *
+ * @author Stanley S.
+ * @version 1.0
+ */
+public class Display extends JPanel
+{
+  // WIP
+}
