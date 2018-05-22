269e16d0ae2cde21f4013fb6d16f80f64a833626
Kevin Chen <kevinjchen4@gmail.com>
2018-05-22 13:46:17 -0700

    peter

---
+++ Cube.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class Cube here.
+ *
+ * @author KChen
+ * @version (a version number or a date)
+ */
+public class Cube
+{
+    // instance variables - replace the example below with your own
+    private Path[][][] cubePaths;
+
+    /**
+     * Constructor for objects of class Cube
+     */
+    public Cube(int sideLength)
+    {
+        cubePaths = new Path[sideLength][sideLength][sideLength];
+    }
+    
+    //public void setPath(int x, int y, int z);
+    {
+        
+    }
+}
---
+++ Path.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Path here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Path
+{
+    private enum PathType{empty, start, end, path}
+    //private PathType type = new PathType();
+    private String color;
+    /**
+     * Constructor for objects of class Path
+     */
+    public Path(PathType inputType, String pathColor)
+    {
+        //type = inputType;
+    }
+
+}
