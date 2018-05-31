3f9c09aaf185e23361d94859a54cc37d0f819dde
Sisson <4878558+StanleyMines@users.noreply.github.com>
2018-05-31 13:15:37 -0700

    Some implementation of the Display and UI for the levels.

--- DisplayLevel.java
+++ DisplayLevel.java
@@ -1,3 +1,8 @@
+import java.awt.geom.Point2D;
+import java.util.List;
+
+import static org.lwjgl.glfw.GLFW.*;
+
 /**
  * class DisplayLevel
  *
@@ -8,4 +13,41 @@
  */
 public class DisplayLevel extends DisplayableWindow
 {
+  Cube cube; // The level that is currently displayed.
+  int level; // Which level to display on the main display. The z-axis. 0 being top.
+
+  /**
+   * Constructor for DisplayLevel.
+   * <p>
+   * Creates a new displayable window with the given cube, starting on the top level.
+   *
+   * @param cube The level data.
+   */
+  public DisplayLevel(Cube cube)
+  {
+    level = 0;
+  }
+  public List<Element> getElements()
+  {
+    return null;
+  }
+
+  public void doClick(int clickType, Point2D location)
+  {
+    if (clickType != GLFW_MOUSE_BUTTON_1)
+      return;
+    if (onAnotherLevel(location))
+      level = onAnotherLevel(location);
+    if (onMainBoard(location))
+      clickedOnSquare getSquare(cube.size(),location);
+  }
+
+  public void doDrag(Point2D location)
+  { }
+
+  public void doRelease(int clickType, Point2D location)
+  { }
+
+  public void doScroll(boolean directionIsUp, Point2D location)
+  { }
 }
