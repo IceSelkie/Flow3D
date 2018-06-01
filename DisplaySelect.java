166c835b7a2b3a7f7e1bd5102ec423c3ec992e9f
Kevin Chen <hicario08@gmail.com>
2018-06-01 14:10:01 -0700

    displayLevel

--- DisplaySelect.java
+++ DisplaySelect.java
@@ -1,11 +1,36 @@
+import java.awt.geom.Point2D;
+import java.util.List;
+
+
+
 /**
  * class DisplaySelect
  *
  * The window data and layout for the window of the menu to select the different layers to play.
  *
- * @author Stanley S. & Kaushik A.
+ * @author Stanley S. & Kaushik A & Kevin C.
  * @version 0.0 (Not Started)
  */
 public class DisplaySelect extends DisplayableWindow
 {
+    public DisplaySelect()
+  {}
+  public void doClick(int clickType, Point2D location)
+  {
+    if (clickType != GLFW_MOUSE_BUTTON_1)
+      return;
+    if (onAnotherLevel(location))
+      level = onAnotherLevel(location);
+    if (onMainBoard(location))
+      clickedOnSquare getSquare(cube.size(),location);
+  }
+  public void doDrag(Point2D location){}
+  public void doRelease(int clickType, Point2D location)
+  {}
+  public void doScroll(boolean directionIsUp, Point2D location)
+  {}
+  public void display(Display.WindowSize w, long window)
+  {
+
+  }
 }
